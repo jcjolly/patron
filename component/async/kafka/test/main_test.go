c045988c4ae44a1dfece458f58bab3f12f09db85
Sotirios Mantziaris <smantziaris@gmail.com>
2020-05-14 22:50:44 +0300

    Added integration test
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

---
+++ component/async/kafka/test/main_test.go
@@ -0,0 +1,60 @@
+// +build integration
+
+package simple
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/beatlabs/patron/component/async"
+	dockerKafka "github.com/beatlabs/patron/test/docker/kafka"
+)
+
+const (
+	topic = "Topic"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(dockerKafka.RunWithKafka(m, 120*time.Second, getTopic(topic)))
+}
+
+func getTopic(name string) string {
+	return fmt.Sprintf("%s:1:1", name)
+}
+
+func getProducerMessage(topic, message string) *sarama.ProducerMessage {
+	return &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+}
+
+func consumeMessages(consumer async.Consumer, expectedMessageCount int) ([]string, error) {
+
+	ctx, cnl := context.WithCancel(context.Background())
+	defer cnl()
+
+	ch, chErr, err := consumer.Consume(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	received := make([]string, 0, expectedMessageCount)
+
+	for {
+		select {
+		case msg := <-ch:
+			received = append(received, string(msg.Payload()))
+			expectedMessageCount--
+			if expectedMessageCount == 0 {
+				return received, nil
+			}
+		case err := <-chErr:
+			return nil, err
+		}
+	}
+}
---
+++ component/async/kafka/test/simple_integration_test.go
@@ -0,0 +1,112 @@
+// +build integration
+
+package simple
+
+import (
+	"testing"
+	"time"
+
+	"github.com/beatlabs/patron/component/async/kafka/simple"
+
+	"github.com/Shopify/sarama"
+	"github.com/beatlabs/patron/component/async/kafka"
+	dockerKafka "github.com/beatlabs/patron/test/docker/kafka"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSimpleConsume(t *testing.T) {
+	sent := []string{"one", "two", "three"}
+	chMessages := make(chan []string)
+	chErr := make(chan error)
+	go func() {
+
+		factory, err := simple.New("test1", topic, dockerKafka.Brokers(), kafka.DecoderJSON(), kafka.Version(sarama.V2_1_0_0.String()),
+			kafka.StartFromNewest())
+		if err != nil {
+			chErr <- err
+			return
+		}
+
+		consumer, err := factory.Create()
+		if err != nil {
+			chErr <- err
+			return
+		}
+		defer func() {
+			_ = consumer.Close()
+		}()
+
+		received, err := consumeMessages(consumer, len(sent))
+		if err != nil {
+			chErr <- err
+			return
+		}
+
+		chMessages <- received
+	}()
+
+	time.Sleep(5 * time.Second)
+
+	prod, err := dockerKafka.NewProducer()
+	require.NoError(t, err)
+	for _, message := range sent {
+		_, _, err = prod.SendMessage(getProducerMessage(topic, message))
+		require.NoError(t, err)
+	}
+
+	var received []string
+
+	select {
+	case received = <-chMessages:
+	case err = <-chErr:
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, sent, received)
+}
+
+func TestSimpleConsume_ClaimMessageError(t *testing.T) {
+	chMessages := make(chan []string)
+	chErr := make(chan error)
+	go func() {
+
+		factory, err := simple.New("test1", topic, dockerKafka.Brokers(), kafka.Version(sarama.V2_1_0_0.String()),
+			kafka.StartFromNewest())
+		if err != nil {
+			chErr <- err
+			return
+		}
+
+		consumer, err := factory.Create()
+		if err != nil {
+			chErr <- err
+			return
+		}
+		defer func() {
+			_ = consumer.Close()
+		}()
+
+		received, err := consumeMessages(consumer, 1)
+		if err != nil {
+			chErr <- err
+			return
+		}
+
+		chMessages <- received
+	}()
+
+	time.Sleep(5 * time.Second)
+
+	prod, err := dockerKafka.NewProducer()
+	require.NoError(t, err)
+	_, _, err = prod.SendMessage(getProducerMessage(topic, "123"))
+	require.NoError(t, err)
+
+	select {
+	case <-chMessages:
+		require.Fail(t, "no messages where expected")
+	case err = <-chErr:
+		require.EqualError(t, err, "could not determine decoder  failed to determine content type from message headers [] : content type header is missing")
+	}
+}
